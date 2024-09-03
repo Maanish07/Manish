46e49e222ba20f8ce83763768c9bd707a50dd6dc
Maanish07 <manishkr88089@gmail.com>
2024-09-03 18:41:22 +0530

    added the cors policy

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,11 @@ app.use(cors());
 app.use(express.json());
 const mongodbURL = process.env.MONGODBURL;
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000",
+    "https://manish-gwap.vercel.app"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
